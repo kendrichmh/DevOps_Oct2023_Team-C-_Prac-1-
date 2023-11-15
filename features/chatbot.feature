955869295fbe118187636acf80ca94fc61ac04ec
Hanisah Musrin <s10226112@connect.np.edu.sg>
2023-11-15 18:28:57 +0800

    feat: completed two scenarios

--- features/chatbot.feature
+++ features/chatbot.feature
@@ -2,9 +2,16 @@ Feature: Test Chatbot interactability
     Background: Common Steps
         Given Chrome browser is Launched
         When Open Toyster page
-    Scenario: Check Chatbot
+
+    #Hanisah - First test case is to check the chatbot, see if can press the suggested questions
+    Scenario: Check Chatbot 
         Then Click Toyster Chatbot
+        Then Click Toyster Chatbot Prompt
         And Close browser
+    
+    #Hanisah - Second test case it to check the product page, see if the filter works properly 
     Scenario: Go Product Page
         Then Select Product
+        Then Select Filter Dropdown
+        Then Check each product
         And Close browser
--- features/steps/chatbot.py
+++ features/steps/chatbot.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 from time import sleep
 
+#Background
 @given(u'Chrome browser is Launched')
 def launchChromeBrowser(context):
     context.driver = webdriver.Chrome()
@@ -13,20 +14,48 @@ def launchChromeBrowser(context):
 def openToysterPage(context):
     context.driver.get("https://toyster.sg")
 
-
+#Scenario 1
 @then(u'Click Toyster Chatbot')
-def verifyChatbot(context):
+def clickChatbot(context):
     sleep(3)
     chatbotFrame = context.driver.find_element(By.XPATH,"//iframe[@title='Shopify online store chat']")
     context.driver.switch_to.frame(chatbotFrame)
     chatbotButton = context.driver.find_element(By.TAG_NAME,'button')
     chatbotButton.click()
+    context.driver.switch_to.default_content()
 
+@then(u'Click Toyster Chatbot Prompt')
+def clickChatbotPrompt(context):
+    sleep(3)
+    chatbotFrame = context.driver.find_element(By.XPATH,"//iframe[@title='Shopify online store chat']")
+    context.driver.switch_to.frame(chatbotFrame)
+    chatbotPromptButton = context.driver.find_element(By.XPATH, '//button/p[text()="What is your operating hour?"]')
+    chatbotPromptButton.click()
+    context.driver.switch_to.default_content()
+
+#Senario 2
 @then(u'Select Product')
 def goToProductPage(context):
     chatbotButton = context.driver.find_element(By.XPATH,"//a[@title='Action Figures & Collectible']")
     chatbotButton.click()
 
+@then(u'Select Filter Dropdown')
+def clickFilter(context):
+    filterButton = context.driver.find_element(By.XPATH,"//select[@name='SortTags']")
+    filterButton.click()
+    filterDropdown = context.driver.find_element(By.XPATH,'//option[text()="Roblox"]')
+    filterDropdown.click()
+
+@then(u'Check each product')
+def checkProduct(context):  
+    search_text = "Roblox"
+    products = context.driver.find_element(By.CLASS_NAME,"product-card__name")
+    if search_text in products.text:
+        print(f"The element contains the {search_text}.")
+    else:
+        print(f"The element does not contain the search text: {search_text}.")  
+    
+#Close browser
 @then(u'Close browser')
 def closeBrowser(context):
     context.driver.close()
