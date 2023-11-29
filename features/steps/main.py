8e6310c3ebd0bdfb4440631dee81f18c508fc0a5
harshini53 <s10204001@connect.np.edu.sg>
2023-11-29 16:13:58 +0800

    Added Comments + Login and Forget Password Test

--- features/steps/main.py
+++ features/steps/main.py
@@ -1,5 +1,8 @@
+# Settings
 from behave import *
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.by import By
@@ -9,6 +12,20 @@ from time import sleep
 options = Options()
 options.add_argument('--ignore-certificate-errors')
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+#Flow
+#-------------------------------------------------------------------------
+# 1.General Steps
+# 2.Register
+# 3.Login
+# 4.Forget Password
+# 5.Search
+# 6.Sort
+# 7.Filter
+# 8.Add to cart
+# 9.Remove from cart
+# 10.Chatbot
+# 11.Customer Review
 
 
 #General Test
@@ -30,6 +47,86 @@ def verifyPageTitle(context):
 def closeBrowser(context):
     context.driver.close()
 
+# Login Test
+@when(u'they click the login link')
+def customerLoginLink(context):
+    customer_login_link = context.driver.find_element(By.ID, 'customer_login_link')
+    customer_login_link.click()
+
+
+@then(u'they should be redirected to the login page')
+def loginSuccess(context):
+    WebDriverWait(context.driver, 10).until(
+    EC.visibility_of_element_located((By.XPATH, "//h1[contains(text(),'Login')]"))
+    )
+    LoginText = context.driver.find_element("xpath", "//h1[contains(text(),'Login')]")
+    ExpectedText = "Login"
+    assert LoginText.text == ExpectedText, "Login not found"
+# -- Scenaio 1 Valid Credentials
+@when(u'the user enters valid credentials username "{user}" and password "{pwd}"')
+def validLogin(context, user, pwd):
+    customerEmail = context.driver.find_element(By.ID, "CustomerEmail")
+    customerPassword = context.driver.find_element(By.ID, "CustomerPassword")
+    customerEmail.send_keys(user)
+    customerPassword.send_keys(pwd)
+
+@when(u'clicks the Sign In Button')
+def clickSignIn(context):
+    customer_SignIn_button = context.driver.find_element(By.XPATH, '//body/div[3]/main[1]/div[1]/div[1]/div[1]/div[1]/div[2]/form[1]/p[1]/input[1]')
+    customer_SignIn_button.click()
+
+@then(u'see My Account Page')
+def seeAccPage(context):
+    WebDriverWait(context.driver, 10).until(
+    EC.visibility_of_element_located((By.XPATH, "//h1[contains(text(),'My Account')]"))
+    )
+    LoginText = context.driver.find_element("xpath", "//h1[contains(text(),'My Account')]")
+    ExpectedText = "My Account"
+    assert LoginText.text == ExpectedText, "My Account not found"
+
+# -- Scenario 2 Invalid Credentials
+@when(u'the user enters invalid credentials username "{user}" and password "{pwd}"')
+def invalidLogin(context, user, pwd):
+    customerEmail = context.driver.find_element(By.ID, "CustomerEmail")
+    customerPassword = context.driver.find_element(By.ID, "CustomerPassword")
+    customerEmail.send_keys(user)
+    customerPassword.send_keys(pwd)
+
+@then(u'they should see an error message')
+def seeError(context):
+    WebDriverWait(context.driver, 10).until(
+    EC.visibility_of_element_located((By.XPATH, "//body/div[4]/main[1]/div[1]/div[1]/div[1]/div[1]/div[2]/form[1]/div[1]"))
+    )
+    LoginText = context.driver.find_element("xpath", "//body/div[4]/main[1]/div[1]/div[1]/div[1]/div[1]/div[2]/form[1]/div[1]")
+    ExpectedText = "Incorrect email or password."
+    assert LoginText.text == ExpectedText, "Error Message not Found"
+
+
+# Forget Password Test
+@when(u'Click Forgot Password')
+def clickForgotPassword(context):
+    customer_ForgotPassword_button = context.driver.find_element(By.ID, 'RecoverPassword')
+    customer_ForgotPassword_button.click()
+
+@when(u'Enter email "{email}" to reset Password')
+def enterForgotPasswordEmail(context,email):
+    forgotPasswordEmail = context.driver.find_element(By.ID, "RecoverEmail")
+    forgotPasswordEmail.send_keys(email)
+
+@when(u'Click Submit')
+def clickSubmit(context):
+    ForgotPasswordSubmit_button = context.driver.find_element(By.XPATH, '//body/div[3]/main[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/form[1]/p[1]/input[1]')
+    ForgotPasswordSubmit_button.click()
+
+@then(u'see We\'ve sent you an email with a link to update your password')
+def seeConfirmation(context):
+    WebDriverWait(context.driver, 10).until(
+    EC.visibility_of_element_located((By.ID, "ResetSuccess"))
+    )
+    ConfirmationText = context.driver.find_element(By.ID, "ResetSuccess")
+    ExpectedText = "We've sent you an email with a link to update your password."
+    assert ConfirmationText.text == ExpectedText, "Error Message not Found"
+
 # Search Test
 @then(u'Input Toyname "{toyName}"')
 def inputToyName(context,toyName):
@@ -50,7 +147,6 @@ def ViewProduct(context):
     sleep(2)
 
 # Sort Test
-
 @then(u'MainMenu Sorting')
 def MainSort(context):
     dropdown = context.driver.find_element(By.XPATH,"//*[@id='SiteNavCompressed']")
